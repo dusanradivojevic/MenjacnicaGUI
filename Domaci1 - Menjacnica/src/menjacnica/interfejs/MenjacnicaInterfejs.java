ad278d16e957015ac6745af73d06e5c569673cb2
Duu1 <r.dusan97@gmail.com>
2018-03-10 19:09:29 +0100

    Izmene u parametrima metoda u interfejsu.

--- Domaci1 - Menjacnica/src/menjacnica/interfejs/MenjacnicaInterfejs.java
+++ Domaci1 - Menjacnica/src/menjacnica/interfejs/MenjacnicaInterfejs.java
@@ -2,10 +2,12 @@ package menjacnica.interfejs;
 
 import java.util.GregorianCalendar;
 
+import menjacnica.Valute.Valuta;
+
 public interface MenjacnicaInterfejs {
 	
-	public boolean dodajKurs(String nazivValute, GregorianCalendar datum);
-	public boolean obrisiKurs(String nazivValute, GregorianCalendar datum);
-	public double vratiKurs(String nazivValute, GregorianCalendar datum);
+	public boolean dodajKurs(Valuta val);
+	public boolean obrisiKurs(Valuta val);
+	public Valuta vratiKurs(String nazivValute, GregorianCalendar datum);
 	
 }
