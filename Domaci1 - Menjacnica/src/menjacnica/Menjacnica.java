247e60e124dcc304d939e964a06315fb8e253f4f
Duu1 <r.dusan97@gmail.com>
2018-03-10 18:26:02 +0100

    Dodate hashCode, toString i equals metode, kao i privatni atribut datum.

--- Domaci1 - Menjacnica/src/menjacnica/Menjacnica.java
+++ Domaci1 - Menjacnica/src/menjacnica/Menjacnica.java
@@ -1,5 +1,14 @@
 package menjacnica;
 
-public class Menjacnica {
+import java.util.LinkedList;
 
+import menjacnica.Valute.Valuta;
+
+public class Menjacnica {
+	
+	private LinkedList<Valuta> valute;
+	
+	public Menjacnica() {
+		valute = new LinkedList<>();
+	}
 }
--- Domaci1 - Menjacnica/src/menjacnica/Valute/Valuta.java
+++ Domaci1 - Menjacnica/src/menjacnica/Valute/Valuta.java
@@ -1,10 +1,19 @@
 package menjacnica.Valute;
 
+import java.util.GregorianCalendar;
+
 public class Valuta {
 	
 	private String naziv, skracenica;
+	private GregorianCalendar datum;
 	private double prodajni, kupovni, srednji; // kurs
-	
+		
+	public GregorianCalendar getDatum() {
+		return datum;
+	}
+	public void setDatum(GregorianCalendar datum) {
+		this.datum = datum;
+	}
 	public String getNaziv() {
 		return naziv;
 	}
@@ -34,6 +43,62 @@ public class Valuta {
 	}
 	public void setSrednji(double srednji) {
 		this.srednji = srednji;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((datum == null) ? 0 : datum.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(kupovni);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((naziv == null) ? 0 : naziv.hashCode());
+		temp = Double.doubleToLongBits(prodajni);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((skracenica == null) ? 0 : skracenica.hashCode());
+		temp = Double.doubleToLongBits(srednji);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Valuta other = (Valuta) obj;
+		if (datum == null) {
+			if (other.datum != null)
+				return false;
+		} else if (!datum.equals(other.datum))
+			return false;
+		if (Double.doubleToLongBits(kupovni) != Double.doubleToLongBits(other.kupovni))
+			return false;
+		if (naziv == null) {
+			if (other.naziv != null)
+				return false;
+		} else if (!naziv.equals(other.naziv))
+			return false;
+		if (Double.doubleToLongBits(prodajni) != Double.doubleToLongBits(other.prodajni))
+			return false;
+		if (skracenica == null) {
+			if (other.skracenica != null)
+				return false;
+		} else if (!skracenica.equals(other.skracenica))
+			return false;
+		if (Double.doubleToLongBits(srednji) != Double.doubleToLongBits(other.srednji))
+			return false;
+		return true;
+	}
+	
+	@Override
+	public String toString() {
+		return "Valuta [Naziv=" + naziv + ", skracenica=" + skracenica + ", datum=" + datum + ", prodajni kurs=" + prodajni
+				+ ", kupovni kurs=" + kupovni + ", srednji kurs=" + srednji + "]";
 	}	
 	
 }
