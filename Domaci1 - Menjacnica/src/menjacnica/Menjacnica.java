68344a89faad4d32ab8a1650aa24a210617a1ff1
Duu1 <r.dusan97@gmail.com>
2018-03-10 19:44:53 +0100

    Odradjena implementacija metoda iz interfejsa.

--- Domaci1 - Menjacnica/src/menjacnica/Menjacnica.java
+++ Domaci1 - Menjacnica/src/menjacnica/Menjacnica.java
@@ -12,20 +12,46 @@ public class Menjacnica implements MenjacnicaInterfejs{
 
 	@Override
 	public boolean dodajKurs(Valuta val) {
-		// TODO Auto-generated method stub
-		return false;
+		if (val == null)
+			throw new RuntimeException("Valuta ne sme biti null!");
+		
+		if (valute.contains(val)) {
+			System.out.println("Valuta vec postoji");
+			return false;
+		}
+		
+		valute.add(val);
+		return true;
 	}
 
 	@Override
 	public boolean obrisiKurs(Valuta val) {
-		// TODO Auto-generated method stub
-		return false;
+		if (!valute.contains(val)) {
+			System.out.println("Lista valuta ne sadrzi unetu valutu!");
+			return false;
+		}
+		
+		for (int i = 0; i < valute.size(); i++) {
+			Valuta v = valute.get(i);
+			
+			if (v.getNaziv().equals(val.getNaziv()) && v.getDatum().equals(val.getDatum()))
+				valute.remove(i);
+		}
+		
+		return true;
 	}
 
 	@Override
-	public Valuta vratiKurs(String nazivValute, GregorianCalendar datum) {
-		// TODO Auto-generated method stub
-		return null;
+	public Valuta vratiKurs(String nazivValute, GregorianCalendar datum) {	
+		
+		for (int i = 0; i < valute.size(); i++) {
+			Valuta v = valute.get(i);
+			
+			if (v.getNaziv().equals(nazivValute) && v.getDatum().equals(datum))
+				return v;
+		}
+		
+		throw new RuntimeException("Ne postoji takva valuta u listi!");
 	}
 	
 }
