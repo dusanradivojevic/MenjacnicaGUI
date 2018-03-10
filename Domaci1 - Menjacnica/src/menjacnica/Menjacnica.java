6a0d2abea950fbc55cbc035e72200b66a225687f
Duu1 <r.dusan97@gmail.com>
2018-03-10 19:34:32 +0100

    Preuzete metode iz intefejsa u klasi Menjacnica

--- Domaci1 - Menjacnica/src/menjacnica/Menjacnica.java
+++ Domaci1 - Menjacnica/src/menjacnica/Menjacnica.java
@@ -1,11 +1,31 @@
 package menjacnica;
 
+import java.util.GregorianCalendar;
 import java.util.LinkedList;
 
 import menjacnica.Valute.Valuta;
+import menjacnica.interfejs.MenjacnicaInterfejs;
 
-public class Menjacnica {
+public class Menjacnica implements MenjacnicaInterfejs{
 	
 	private LinkedList<Valuta> valute = new LinkedList<>();
+
+	@Override
+	public boolean dodajKurs(Valuta val) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean obrisiKurs(Valuta val) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Valuta vratiKurs(String nazivValute, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
 }
